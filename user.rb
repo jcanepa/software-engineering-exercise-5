2c6e2baad4cff82d986fef887076991b91b53e1e
Julian Canepa <julian.l.canepa@gmail.com>
2022-10-24 10:59:28 -0700

    Remove un used method bodies.

--- user.rb
+++ user.rb
@@ -3,11 +3,7 @@ class User
     puts "user"
   end
 
-  def self.find(id)
-    return 'foo'
-  end
+  def self.find(id); end
 
-  def self.create(email=nil, password=nil)
-    return 'foo'
-  end
+  def self.create(email=nil, password=nil); end
 end
